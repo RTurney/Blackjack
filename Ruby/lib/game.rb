a46b7167690b302ecfa3f3a213bcdbad9868c699
RTurney <r.turney93@gmail.com>
2021-03-14 20:11:46 +0000

    Refactor compare_hands function of Game class

--- Ruby/lib/game.rb
+++ Ruby/lib/game.rb
@@ -1,8 +1,18 @@
+require_relative './player'
 
  class Game
 
+   def initialize
+     @player = Player.new
+     @dealer = Player.new
+   end
+
    def compare_hands
-     'dealer'
+     if @player.score > @dealer.score
+       'Player'
+     else
+       'dealer'
+     end
    end
 
  end
