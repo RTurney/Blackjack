83fa04e425d40dffe5590f89887c5a376b2bbb93
RTurney <r.turney93@gmail.com>
2021-03-14 19:37:29 +0000

    Refactor score method

--- Ruby/lib/player.rb
+++ Ruby/lib/player.rb
@@ -1,16 +1,13 @@
 
 class Player
-  attr_reader :hand
+  attr_reader :hand, :score
 
   def initialize
     @hand = []
+    @score = 0
   end
 
   def receive_cards(card)
     hand.push(card)
-  end
-
-  def score
-    0
   end
 end
