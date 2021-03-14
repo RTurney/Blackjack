52c2dcdac229cc0d5ae226c9b6716823cbc27b39
RTurney <r.turney93@gmail.com>
2021-03-14 19:43:39 +0000

    Add calculate_score to Player class

--- Ruby/lib/player.rb
+++ Ruby/lib/player.rb
@@ -10,4 +10,12 @@ class Player
   def receive_cards(card)
     hand.push(card)
   end
+
+  def calculate_score
+    sum = 0
+    hand.each do |number|
+      sum += number
+    end
+    sum
+  end
 end
--- Ruby/spec/player_spec.rb
+++ Ruby/spec/player_spec.rb
@@ -9,7 +9,6 @@ describe Player do
   end
 
   describe '.receive_cards' do
-
     it "will push a card into the players hand" do
       subject.receive_cards(3)
       expect(subject.hand).to eq [3]
@@ -19,6 +18,13 @@ describe Player do
   describe '.score' do
     it "will have a score of 0 on initialisation" do
       expect(subject.score).to eq 0
+    end
+  end
+
+  describe '.calculate_score' do
+    it "will return the total score of the player's hand" do
+      subject.receive_cards(1)
+      expect(subject.calculate_score).to eq 1
     end
   end
 end
